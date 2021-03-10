37da9d7be112d3b79b4dc4627d5e13c61c0f9773
anaca <anaca@DESKTOP-LK8J681>
2021-03-09 21:10:11 -0300

    Criação do projeto.

---
+++ src/main/java/br/edu/iff/ProjetoControleEstoque/ProjetoControleEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ProjetoControleEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControleEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControleEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/ProjetoControleEstoque/ProjetoControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ProjetoControleEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
